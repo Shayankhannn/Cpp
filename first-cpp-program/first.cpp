c6d0c759764440496dd2c7e81dd4099729bf4eb7
shayankhannn <shayannkhannn007@gmail.com>
2026-04-11 02:29:44 -0700

    first cpp program

--- first-cpp-program/first.cpp
+++ first-cpp-program/first.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-
-    std::cout << "hello" << std::endl;
+    std::cout << "hello World" << std::endl;
+    cout << "number 1" << endl;
+    cout << "number 2" << endl;
+    return 0;
 }
